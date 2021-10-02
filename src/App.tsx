87aa710dc5ab9d61914b3bf6ecef1e996707c003
Zack <zack.valentine22@gmail.com>
2021-10-01 21:53:13 -0500

    API test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [apiResponse, setApiResponse] = useState<string>();
+
+  useEffect(() => {
+      const url = 'http://theos-books-api.herokuapp.com/hello';
+
+      const fetchData = async() => {
+          try {
+              const response = await fetch(url, {
+                  headers: {
+                      'Accept': `application/json`,
+                      'Content-Type': `application/json`
+                  }
+              });
+              const text = await response.text();
+              console.log(text);
+              setApiResponse(text);
+          } catch (error) {
+              console.error("error: ", error);
+          }
+      }
+
+      fetchData();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {apiResponse}
         </p>
         <a
           className="App-link"
